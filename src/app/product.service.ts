674b389e248b32174f95233870c4fa8676eca4e3
Elias Chebib <elias.chebib@gmail.com>
2019-02-02 20:44:20 +0100

    part4 + commenting a test because of newer version changes

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient  } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+
 import 'rxjs/add/operator/map';
+import {Album} from './album';
 
 @Injectable()
 export class ProductService {
@@ -8,7 +11,7 @@ export class ProductService {
   private _albumUrl = '../assets/album.json';
   constructor(private _http : HttpClient) { }
 
-  public getAlbum(id : number){
-    return this._http.get(this._albumUrl);
+  public getAlbum(id : number) : Observable<Album>{
+    return this._http.get<Album>(this._albumUrl);
   }
 }
